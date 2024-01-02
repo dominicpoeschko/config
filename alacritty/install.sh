6e502e0c17a16a41dcadc51447aea478122d05ce
dominic <mail@dominicp.de>
2024-01-02 12:49:49 +0100

    alacrity update

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/sh
 sudo pacman -S --needed alacritty ttf-hack-nerd ttf-hack
 mkdir -p ~/.config/alacritty
-ln -sf "$PWD"/alacritty.yml ~/.config/alacritty/
+ln -sf "$PWD"/alacritty.toml ~/.config/alacritty/
