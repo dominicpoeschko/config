969c00905632c05a36eb3fb4450cea6ffae33667
NPierick <NPierick@breuer-motoren.de>
2024-07-29 15:56:54 +0200

    Update gitsigns

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -19,12 +19,13 @@ require('telescope').load_extension("smart_open")
 require('gitsigns').setup {
     signs = {
         -- TODO add hl to colorscheme
-        add          = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change       = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete       = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete    = { hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    },
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+       },
     numhl = false,
     linehl = false,
     watch_gitdir = {
