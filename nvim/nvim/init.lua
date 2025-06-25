07c7324b558b3db4ec615502f5f0f2a26ae73571
Dominic Pöschko <dominic.poeschko@apsensing.com>
2025-06-25 12:45:30 +0000

    fix cmd

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -18,11 +18,11 @@ vim.cmd('set nospell')
 
 require("config.lazy")
 
-vim.keymap.set({"n", "v"}, "<leader>r", ":%s///gc<Left><Left><Left><Left>", {desc = "Search and Replace", silent = false})
+vim.keymap.set("n", "<leader>r", ":%s///gc<Left><Left><Left><Left>", {desc = "Search and Replace", silent = false})
 vim.keymap.set("n", "<leader>f", ':Telescope smart_open<CR>', {noremap = true, silent = true })
 vim.keymap.set("n", "<leader>d", ':Telescope find_files<CR>', {noremap = true, silent = true })
-vim.keymap.set("n", "<leader>s", ':Telescope live_grep<CR>', {noremap = true, silent = true })
-vim.keymap.set("n", "<leader>r", ':Telescope grep_string<CR>', {noremap = true, silent = true })
+vim.keymap.set("n", "<leader>x", ':Telescope live_grep<CR>', {noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", ':Telescope grep_string<CR>', {noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {desc = "Go to Definition"})
 vim.keymap.set("n", "<leader>gc", "<cmd>lua vim.lsp.buf.declaration()<CR>", {desc = "Go to Declaration"})
