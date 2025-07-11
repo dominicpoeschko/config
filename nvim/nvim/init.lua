0a1bfa60438bc0cacf3d0cee9833f7a2c9dc5f6a
dominic <mail@dominicp.de>
2025-07-11 13:07:19 +0200

    cleanup

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -34,70 +34,13 @@ vim.keymap.set('n', '<F5>', require('dap').continue)
 vim.keymap.set('n', '<F10>', require('dap').step_over)
 vim.keymap.set('n', '<F11>', require('dap').step_into)
 vim.keymap.set('n', '<F12>', require('dap').step_out)
-vim.keymap.set('n', '<leader>b', require('dap').toggle_breakpoint)
 
-vim.keymap.set('n', '<leader>du', require('dapui').toggle)
-
+vim.keymap.set('n', '<leader>wb', require('dap').toggle_breakpoint)
+vim.keymap.set('n', '<leader>wu', require('dapui').toggle)
 
 require("which-key").add({"<leader>l", group = "+LSP"})
 require("which-key").add({"<leader>g", group = "+Goto"})
-
-local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
-}
-local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
-
-vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup { scope = { highlight = highlight } }
-
-hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
-require("lspconfig").clangd.setup {
-    cmd = {
-        "clangd",
-        "--header-insertion=never",
-        "--compile-commands-dir=build",
-        "--all-scopes-completion",
-        "--background-index"
-    },
-    handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = false,
-            signs = true,
-            underline = true,
-            update_in_insert = true
-        })
-    }
-}
-
-require("lspconfig").cmake.setup {}
-require("lspconfig").yamlls.setup {}
-require("lspconfig").jsonls.setup {
-    cmd = {
-        "vscode-json-languageserver",
-        "--stdio"
-    },
-}
-require("lspconfig").pylsp.setup {}
-
-require('dap-go').setup()
+require("which-key").add({"<leader>w", group = "+Debug"})
 
 vim.cmd('colorscheme gruvbox')
 
